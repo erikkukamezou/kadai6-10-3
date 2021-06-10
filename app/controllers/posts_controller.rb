a20003880d20d2edfc4f4b7c0d249464fd179788
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-10 14:56:18 +0000

    新規作成の画面を更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
-    @post = Post.all
+    @posts = Post.all
   end
   def new
     @post = Post.new
@@ -8,7 +9,7 @@ class PostsController < ApplicationController
   def create
     @post =Post.new(post_params)
     if @post.save
-      redirect_to new_posts_path,notice:"ブログを作成しました!"
+      redirect_to new_post_path,notice:"ブログを作成しました!"
     else
       render :new
     end
@@ -21,14 +22,17 @@ class PostsController < ApplicationController
   end
   def update
      @post = Post.find(params[:id])
-    if @blog.update(post_params)
-      redirect_to posts_path, notice: "ブログを編集しました！"
+    if @post.update(post_params)
+      redirect_to post_path, notice: "ブログを編集しました！"
     else
       render :edit
     end
   end
   private
-  def blog_params
+  def post_params
     params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
