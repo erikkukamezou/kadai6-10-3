8351eac4aa8af6edd2bd858930111a7982e3def2
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-10 13:48:37 +0000

    一覧画面の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
   def new
+    @post = Post.new
+  end
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
+  end
+  def show
+     @post = Post.find(params[:id])
+  end
+  private
+  def blog_params
+    params.require(:post).permit(:content)
   end
 end
