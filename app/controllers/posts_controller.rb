b7bea1998d32bd19485ccb65ff3b3446ea4357c9
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-10 14:13:52 +0000

    編集画面の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post =Post.new(post_params)
+    if @post.save
+      redirect_to new_posts_path,notice:"ブログを作成しました!"
+    else
+      render :new
+    end
   end
   def show
      @post = Post.find(params[:id])
+  end
+  def edit
+     @post =Post.find(params[:id])
+  end
+  def update
+     @post = Post.find(params[:id])
+    if @blog.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
   private
   def blog_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :content, presence: true, length: { in: 1..140 }
+  validates :content, length: { maximum: 140 }
+  validates :content, presence: true
 end
