c3f05678447dfd3c6807562cdafd4e4d8e6d8b55
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-10 16:36:21 +0000

    削除画面の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -15,18 +15,22 @@ class PostsController < ApplicationController
     end
   end
   def show
-     @post = Post.find(params[:id])
+     # @post = Post.find(params[:id])
   end
   def edit
-     @post =Post.find(params[:id])
+     # @post =Post.find(params[:id])
   end
   def update
-     @post = Post.find(params[:id])
+     # @post = Post.find(params[:id])
         if @post.update(post_params)
           redirect_to posts_path, notice: "ブログを編集しました！"
         else
           render :edit
         end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"ブログを削除しました！"
   end
   private
   def post_params
