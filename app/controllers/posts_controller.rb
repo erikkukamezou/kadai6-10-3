7ebc7968543e9e33d7434a2e5778e5b3d7709eb8
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-10 16:11:10 +0000

    編集画面の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,11 +22,11 @@ class PostsController < ApplicationController
   end
   def update
      @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to post_path, notice: "ブログを編集しました！"
-    else
-      render :edit
-    end
+        if @post.update(post_params)
+          redirect_to posts_path, notice: "ブログを編集しました！"
+        else
+          render :edit
+        end
   end
   private
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :content, presence: true, length: { in: 1..140 }
   validates :content, length: { maximum: 140 }
-  validates :content, presence: true
+  # validates :content, presence: true
 end
